a00d8a58fcab445dc382d10e1c3f08a32fca5e9f
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-01-19 18:53:15 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,7 +3,7 @@ local gameid = {
 }
 
 if gameid then
-    if game.PlaceId == gameid[1] or game.PlaceId == 5253186791 then
+    if game.PlaceId == gameid[1] or game.PlaceId == 5253186791 then -- id 2 is for tower of hell appeal
            loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Toh.lua"))()
         else
             game.Players.LocalPlayer:Kick("The Game Isnt Supported to the script")
