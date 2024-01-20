26ab3f4db3df180a4aaf8f2bfdb4073b71894601
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-01-20 12:10:36 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,28 @@
 local gameid = {
     [1] = 1962086868, -- Tower Of Hell
+    [2] = 13864661000, -- Break in 2 (lobby)
 }
 
+function notif(title, txt, dur)
+    game.StarterGui:SetCore("SendNotification", {
+    Title = title;
+    Text = txt;
+    Duration = dur;
+})
+end
+
 if gameid then
-    if game.PlaceId == gameid[1] or game.PlaceId == 5253186791 then -- id 2 is for tower of hell appeal
+    --- Tower Of Hell
+    if game.PlaceId == gameid[1] or game.PlaceId == 5253186791 then
            loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Toh.lua"))()
         else
-            game.Players.LocalPlayer:Kick("The Game Isnt Supported to the script")
+            notif("Game Not Supported", "This Hub Isnt Supported for this game.", 5)
     end
+    ----- Break In 2
+    if game.PlaceId == gameid[2] or game.PlaceId == 13864667823 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Breakin2.lua"))()
+        else
+            notif("Game Not Supported", "This Hub Isnt Supported for this game.", 5)
+    end
+    -- Soon
 end
