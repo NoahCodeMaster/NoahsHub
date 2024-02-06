5d4cfabf575f187e2f4df5e5b2c8fce023ca12b6
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-02-06 16:42:17 +0200

    Update Toh.lua

--- Games/Toh.lua
+++ Games/Toh.lua
@@ -131,11 +131,24 @@ inf:AddButton({
 })
 
 set:AddButton({
+    Name = "Discord Invite Link",
+    Callback = function()
+      setclipboard("https://discord.gg/m4jMgc8pga")
+    end
+})
+
+set:AddButton({
+    Name = "File Loader Setup (Soon)",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/VentureHub/main/Settings/Source.lua"))()
+    end
+})
+
+set:AddButton({
     Name = "Destroy Gui",
     Callback = function()
         OrionLib:Destroy()
-    end
-})
+end
 
 sup:AddParagraph("Supported Games",game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/VentureHub/main/SupportedGames/games.sup"))
 
