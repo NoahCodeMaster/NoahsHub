f2739f9acb850738ed25cec3d1d74ce1ec88bf77
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-02-13 01:54:51 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,30 +1,30 @@
 local gameid = { 
-    [1] = 1962086868, -- Tower Of Hell
-    [2] = 13864661000, -- Break in 2 (lobby)
-    [3] = 3956818381 -- Ninja Legends
+    [1962086868] = true, -- Tower Of Hell
+    [13864661000] = true, -- Break in 2 (lobby)
+    [3956818381] = true -- Ninja Legends
 }
 
 function notif(title, txt, dur)
     game.StarterGui:SetCore("SendNotification", {
-    Title = title;
-    Text = txt;
-    Duration = dur;
-})
+        Title = title;
+        Text = txt;
+        Duration = dur;
+    })
 end
 
-if gameid then
-    --- Tower Of Hell
-    if game.PlaceId == gameid[1] or game.PlaceId == 5253186791 then
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Toh.lua"))()
-        else
-    end
-    ----- Break In 2
-    if game.PlaceId == gameid[2] or game.PlaceId == 13864667823 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Breakin2.lua"))()
-        else
-    end
-    if game.PlaceId == gameid[3] then
+-- Check if the current game ID is supported
+if not gameid[game.PlaceId] then
+    notif("Unsupported Game", "This game is not supported.", 5)
+else
+    -- Tower Of Hell
+    if game.PlaceId == 1962086868 or game.PlaceId == 5253186791 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Toh.lua"))()
+    -- Break In 2
+    elseif game.PlaceId == 13864661000 or game.PlaceId == 13864667823 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Breakin2.lua"))()
+    -- Ninja Legends
+    elseif game.PlaceId == 3956818381 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Ninjalegends.lua"))()
     end
-    -- Soon
 end
+-- Soon
