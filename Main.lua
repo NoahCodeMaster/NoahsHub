2d3d03c85957f766d5c3c6cddec6eda93de31525
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-02-13 13:16:17 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,7 @@
 local gameid = { 
     [1962086868] = true, -- Tower Of Hell
+    [1962086868] = true, -- Tower Of Hell Pro
+    [5253186791] = true, -- Tower Of Hell Banned
     [13864661000] = true, -- Break in 2 (lobby)
     [13864667823] = true, -- break in 2 game
     [3956818381] = true -- Ninja Legends
@@ -18,7 +20,7 @@ if not gameid[game.PlaceId] then
     notif("Unsupported Game", "This script does not support this game.", 5)
 else
     -- Tower Of Hell
-    if game.PlaceId == 1962086868 or game.PlaceId == 5253186791 then
+    if game.PlaceId == 1962086868 or game.PlaceId == 5253186791 or game.PlaceId == 1962086868 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NoahCodeMaster/NoahsHub/main/Games/Toh.lua"))()
     -- Break In 2
     elseif game.PlaceId == 13864661000 or game.PlaceId == 13864667823 then
