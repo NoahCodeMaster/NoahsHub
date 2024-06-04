5ac82a82767e6fc2fd36d06e9c120195f2bdb53a
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-06-04 06:49:51 +0300

    Create generateKey.js

---
+++ api/generateKey.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+let keys = {};
+
+module.exports = (req, res) => {
+    const { hwid } = req.query;
+    if (!hwid) {
+        return res.status(400).send('HWID is required');
+    }
+
+    const key = uuidv4().replace(/-/g, '').substring(0, 16); // Generate a 16-char hex key
+    const expiration = Date.now() + 24 * 60 * 60 * 1000; // 24 hours from now
+
+    keys[hwid] = { key, expiration };
+
+    res.json({ key });
+};
