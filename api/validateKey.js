b738acbb84e14280d2d6839fb8dc01e7c1c91b6d
$Noah <127032836+NoahCodeMaster@users.noreply.github.com>
2024-06-04 13:39:05 +0300

    Update validateKey.js

--- api/validateKey.js
+++ api/validateKey.js
@@ -1,3 +1,5 @@
+let keys = {};
+
 module.exports = (req, res) => {
     const { hwid, key } = req.query;
     if (!hwid || !key) {
@@ -5,9 +7,13 @@ module.exports = (req, res) => {
     }
 
     const storedKey = keys[hwid];
-    if (storedKey && storedKey.key === key && storedKey.expiration > Date.now()) {
-        return res.json({ valid: true });
+    if (!storedKey) {
+        return res.status(404).send('Key not found');
     }
 
-    res.json({ valid: false });
+    if (storedKey.key === key && storedKey.expiration > Date.now()) {
+        res.json({ valid: true });
+    } else {
+        res.json({ valid: false });
+    }
 };
